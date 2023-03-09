ace14719020604d7ec764485fee622be2fe45a35
Ninad Khargonkar <ninad.khargonkar@gmail.com>
2023-03-08 23:39:11 -0600

    code formatting with black

--- chess_gif.py
+++ chess_gif.py
@@ -10,23 +10,24 @@ import imageio
 
 from text_to_img import text_image
 
-TMP_DIR = './.temp/'
+TMP_DIR = "./.temp/"
+
 
 def create_txt_files(pgn_file: str):
-    ''' Creates a bunch of text files corresponding to every move in the game
+    """Creates a bunch of text files corresponding to every move in the game
     Input:
         pgn_file: path to the .pgn file for the chess game
-    '''
-    with open(pgn_file, 'r') as pgn:
+    """
+    with open(pgn_file, "r") as pgn:
         first_game = chess.pgn.read_game(pgn)
     # create a temp directory for txt files. Delete it later.
     os.mkdir(TMP_DIR)
     board = first_game.board()
     # starting with 10 to avoid problems with 1 and 10 while sorting file names
-    counter = 10    
+    counter = 10
     for move in first_game.mainline_moves():
-        fname = os.path.join(TMP_DIR, f'{counter}_file.txt') 
-        with open(fname, 'w') as f:
+        fname = os.path.join(TMP_DIR, f"{counter}_file.txt")
+        with open(fname, "w") as f:
             print(board, file=f)
         board.push(move)
         counter += 1
@@ -34,50 +35,54 @@ def create_txt_files(pgn_file: str):
 
 def create_img_files():
     files = os.listdir(TMP_DIR)
-    tmp_img_dir = os.path.join(TMP_DIR, 'imgs')
-    os.mkdir(tmp_img_dir) # temp folder for images
+    tmp_img_dir = os.path.join(TMP_DIR, "imgs")
+    os.mkdir(tmp_img_dir)  # temp folder for images
     for f in files:
-        image = text_image(os.path.join(TMP_DIR, f)) 
-        imgfile = f'{f}_img.png'
+        image = text_image(os.path.join(TMP_DIR, f))
+        imgfile = f"{f}_img.png"
         image.save(os.path.join(tmp_img_dir, imgfile))
 
 
 def create_gif(gif_name: str, gif_duration: int):
-    ''' Reads the temp image files created and glues together a gif file. Also cleans up
+    """Reads the temp image files created and glues together a gif file. Also cleans up
     the tmp files.
 
     Input:
         gif_name: output gif file path
         gif_duration: how many seconds should each frame last?
-    '''
-    imgfolder = os.path.join(TMP_DIR, 'imgs')
+    """
+    imgfolder = os.path.join(TMP_DIR, "imgs")
     fnames = os.listdir(imgfolder)
-    images=[]
-    fnames.sort()  # So that images for moves are in order    
+    images = []
+    fnames.sort()  # So that images for moves are in order
     for f in fnames:
-        images.append(imageio.imread(os.path.join(imgfolder, f)))    
-    imageio.mimsave(gif_name, images, duration=gif_duration) # Save the list of images as a gif
-    shutil.rmtree(TMP_DIR) # Remove the temp txt and png files.
+        images.append(imageio.imread(os.path.join(imgfolder, f)))
+    imageio.mimsave(
+        gif_name, images, duration=gif_duration
+    )  # Save the list of images as a gif
+    shutil.rmtree(TMP_DIR)  # Remove the temp txt and png files.
+
 
 def make_args():
     ap = argparse.ArgumentParser(
-        prog = 'chess_gif',
-        description = 'Convert a PGN file for a chess game to a gif showing the moves made.',
-        epilog='Not at all professional code, use at your own risk!')
-    
-    ap.add_argument("-d", "--duration", required=True,
-        help="Time delay between frames of gif")
-    ap.add_argument("-i", "--input", required=True,
-        help="Input PGN file")
-    ap.add_argument("-o", "--output", required=True,
-        help="Output GIF file")
+        prog="chess_gif",
+        description="Convert a PGN file for a chess game to a gif showing the moves made.",
+        epilog="Not at all professional code, use at your own risk!",
+    )
+
+    ap.add_argument(
+        "-d", "--duration", required=True, help="Time delay between frames of gif"
+    )
+    ap.add_argument("-i", "--input", required=True, help="Input PGN file")
+    ap.add_argument("-o", "--output", required=True, help="Output GIF file")
     args = ap.parse_args()
     return args
+
 
 def main(args):
     pgn_file = args.input
     gif_file = args.output
-    frame_duration = args.duration # time for each frame in gif
+    frame_duration = args.duration  # time for each frame in gif
 
     if os.path.exists(TMP_DIR):
         print(f"[ERROR] Delete the temp dir ({TMP_DIR}) and retry!")
@@ -85,14 +90,14 @@ def main(args):
 
     print("Creating temp text files \n")
     create_txt_files(pgn_file)
-    
+
     print("Creating temp img files \n")
     create_img_files()
-    
+
     print("Creating gif file \n")
     create_gif(gif_file, frame_duration)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = make_args()
     main(args)
--- text_to_img.py
+++ text_to_img.py
@@ -1,8 +1,8 @@
-'''
+"""
 Python script to read a text file and create its image
 Credits: `KobeJohn` on StackOverflow
 Link to post/comment: https://stackoverflow.com/q/29760402/9579260
-'''
+"""
 
 import PIL
 import PIL.Image
@@ -23,31 +23,33 @@ def text_image(text_path: str, font_path=None) -> PIL.Image:
     Returns:
         image: PIL.Image (grayscale) equivalent of the input text file
     """
-    grayscale = 'L'
+    grayscale = "L"
     # parse the file into lines
     with open(text_path) as text_file:  # can throw FileNotFoundError
         lines = tuple(l.rstrip() for l in text_file.readlines())
 
     # choose a font (you can see more detail in my library on github)
     large_font = 20  # get better resolution with larger size
-    font_path = font_path or 'cour.ttf'  # Courier New. works in windows. linux may need more explicit path
+    font_path = (
+        font_path or "cour.ttf"
+    )  # Courier New. works in windows. linux may need more explicit path
     try:
         font = PIL.ImageFont.truetype(font_path, size=large_font)
     except IOError:
         font = PIL.ImageFont.load_default()
-        print('Could not use chosen font. Using default.')
+        print("Could not use chosen font. Using default.")
 
     # make the background image based on the combination of font and lines
     pt2px = lambda pt: int(round(pt * 96.0 / 72))  # convert points to pixels
     max_width_line = max(lines, key=lambda s: font.getsize(s)[0])
     # max height is adjusted down because it's too large visually for spacing
-    test_string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    test_string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     max_height = pt2px(font.getsize(test_string)[1])
     max_width = pt2px(font.getsize(max_width_line)[0])
-    height = max_height * (len(lines))   # perfect or a little oversized
-    width = int(round(max_width))   # a little oversized
+    height = max_height * (len(lines))  # perfect or a little oversized
+    width = int(round(max_width))  # a little oversized
     image = PIL.Image.new(grayscale, (width, height), color=PIXEL_OFF)
-    
+
     draw = PIL.ImageDraw.Draw(image)
 
     # draw each line of text
@@ -55,8 +57,9 @@ def text_image(text_path: str, font_path=None) -> PIL.Image:
     horizontal_position = 5
     line_spacing = int(round(max_height * 0.8))  # reduced spacing seems better
     for line in lines:
-        draw.text((horizontal_position, vertical_position),
-                  line, fill=PIXEL_ON, font=font)
+        draw.text(
+            (horizontal_position, vertical_position), line, fill=PIXEL_ON, font=font
+        )
         vertical_position += line_spacing
     # crop the text
     c_box = PIL.ImageOps.invert(image).getbbox()
